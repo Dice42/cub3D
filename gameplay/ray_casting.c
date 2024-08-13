ed6b7e47c84bd1dfaa17d9eeca333c85397d6b48
Dice42 <mohammoh@student.42abudhabi.ae>
2024-08-13 18:18:06 +0400

    merged salam with master

--- gameplay/ray_casting.c
+++ gameplay/ray_casting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ray_casting.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ssibai < ssibai@student.42abudhabi.ae>     +#+  +:+       +#+        */
+/*   By: mohammoh <mohammoh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/26 17:12:13 by mohammoh          #+#    #+#             */
-/*   Updated: 2024/08/13 18:14:41 by ssibai           ###   ########.fr       */
+/*   Updated: 2024/08/13 18:16:48 by mohammoh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,9 @@ void	reset_angles(float *angle)
  */
 void draw_3d_rays(t_cub3d *cube)
 {
+	int	i;
+
+	i = 0;
 	cube->player.rays.rx = cube->player.transform.x0 + 3;
 	cube->player.rays.ry = cube->player.transform.y0 + 3;
 	if (cube->player.transform.angle == 1.5)
