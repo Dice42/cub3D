4a78cf0beda51b13cc0db27bfda54c237e57b30b
salam <salamsibai96@gmail.com>
2024-08-14 10:18:12 +0400

    restored

---
+++ init/init.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssibai < ssibai@student.42abudhabi.ae>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/16 16:00:30 by ssibai            #+#    #+#             */
+/*   Updated: 2024/07/28 12:23:27 by ssibai           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/cub3D.h"
+
+bool	init_cube(t_cub3d *cube)
+{
+	ft_bzero(&cube->data, sizeof(t_mlx_data));
+	ft_bzero(&cube->level, sizeof(t_level));
+	ft_bzero(&cube->player, sizeof(t_player));
+	ft_bzero(&cube->player.transform, sizeof(cube->player.transform));
+	ft_bzero(&cube->data.img, sizeof(cube->data.img));
+	return (true);
+}
